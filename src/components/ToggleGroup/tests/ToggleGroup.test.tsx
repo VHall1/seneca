2116e27228b0256411190d6ecc814d5cffddbd51
vhall1 <victorhallpsn@hotmail.com>
2025-02-20 13:15:07 +0000

    fix tests

--- src/components/ToggleGroup/tests/ToggleGroup.test.tsx
+++ src/components/ToggleGroup/tests/ToggleGroup.test.tsx
@@ -15,6 +15,7 @@ describe("<ToggleGroup />", () => {
   test("locks once all correct options have been selected", async () => {
     render(<MockToggleGroup />);
 
+    const mockQuestionOptions = mockQuestions[0].questionOptions;
     for (const questionOption of mockQuestionOptions) {
       const correct = questionOption.options.find(
         (o) => o.value === questionOption.correct
@@ -36,60 +37,61 @@ describe("<ToggleGroup />", () => {
   });
 });
 
-const mockQuestionOptions = [
+const mockQuestions = [
   {
-    id: "73b5f49f9002a82a",
-    correct: "hot",
-    options: [
+    id: "48c5fffa818be298",
+    title: "An animal cell contains:",
+    questionOptions: [
       {
-        value: "hot",
-        label: "Hot",
+        id: "73b5f49f9002a82a",
+        correct: "hot",
+        options: [
+          {
+            value: "hot",
+            label: "Hot",
+          },
+          {
+            value: "cold",
+            label: "Cold",
+          },
+        ],
       },
       {
-        value: "cold",
-        label: "Cold",
-      },
-    ],
-  },
-  {
-    id: "1fdf163d4ec2c598",
-    correct: "option2",
-    options: [
-      {
-        value: "option1",
-        label: "Option 1",
-      },
-      {
-        value: "option2",
-        label: "Option 2",
-      },
-      {
-        value: "option3",
-        label: "Option 3",
-      },
-    ],
-  },
-  {
-    id: "98d477d470d05368",
-    correct: "impermeable",
-    options: [
-      {
-        value: "partially",
-        label: "Partially permeable membrane",
+        id: "1fdf163d4ec2c598",
+        correct: "option2",
+        options: [
+          {
+            value: "option1",
+            label: "Option 1",
+          },
+          {
+            value: "option2",
+            label: "Option 2",
+          },
+          {
+            value: "option3",
+            label: "Option 3",
+          },
+        ],
       },
       {
-        value: "impermeable",
-        label: "Impermeable membrane",
+        id: "98d477d470d05368",
+        correct: "impermeable",
+        options: [
+          {
+            value: "partially",
+            label: "Partially permeable membrane",
+          },
+          {
+            value: "impermeable",
+            label: "Impermeable membrane",
+          },
+        ],
       },
     ],
   },
 ];
 
 function MockToggleGroup() {
-  return (
-    <ToggleGroup
-      title="An animal cell contains:"
-      questionOptions={mockQuestionOptions}
-    />
-  );
+  return <ToggleGroup questions={mockQuestions} />;
 }
